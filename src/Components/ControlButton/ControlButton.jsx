081eaec11aad7e078565c540bf449df4092cfe22
sivin23 <sivin.piechartdigital@gmail.com>
2023-08-17 08:53:23 +0530

    Control button style changed

--- src/Components/ControlButton/ControlButton.jsx
+++ src/Components/ControlButton/ControlButton.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import './ControlButton.css'
 
-function ControlButton() {
+function ControlButton({controlButtons}) {
   return (
-    <div>ControlButton</div>
+    <>
+    <ul className="nav nav-pills">
+        {
+          controlButtons.map(Item => (
+            <li class="nav-item">
+              <button className={`nav-link ${Item.activeState? "control-btn-active":" " } control-btn`} aria-current="page">{Item.title}</button>
+            </li>
+          ))
+        }
+        </ul>
+    </>
   )
 }
 
--- src/Pages/Booking.jsx
+++ src/Pages/Booking.jsx
@@ -1,8 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ControlButton from '../Components/ControlButton/ControlButton';
 
 function Booking() {
+  const [controlButtons, setControlButtons] = useState([
+    { 
+      title: "Ticket Booking",
+      activeState: true,
+      addBtn: true,
+      addBtnTitle: "Add Ticket",
+      ShowDataURL: "/ticket",
+      AddDataURL:"/not-set"
+    },
+    { 
+      title: "Tour Booking",
+      activeState: false,
+      addBtn: true,
+      addBtnTitle: "",
+      ShowDataURL: "/ticket",
+      AddDataURL:"/not-set"
+    },
+    { 
+      title: "Block Seats",
+      activeState: false,
+      addBtn: false,
+      addBtnTitle: "",
+      ShowDataURL: "/ticket",
+      AddDataURL:"/not-set"
+    },
+    { 
+      title: "Unblock Seat",
+      activeState: false,
+      addBtn: false,
+      addBtnTitle: "",
+      ShowDataURL: "/ticket",
+      AddDataURL:"/not-set"
+    },
+    { 
+      title: "Club Service",
+      activeState: false,
+      addBtn: false,
+      addBtnTitle: "",
+      ShowDataURL: "/ticket",
+      AddDataURL:"/not-set"
+    },
+    { 
+      title: "TrackTicket",
+      activeState: false,
+      addBtn: false,
+      addBtnTitle: "",
+      ShowDataURL: "/ticket",
+      AddDataURL:"/not-set"
+    }
+  ]);
+  const [bookingFilters, setBookingFilters] = useState([
+    {
+      bookTicketFilters: [
+        {
+        filterTitle: "Category",
+        type: "dropdown",
+        options:["AC","NAC"],
+        }
+  ]}
+  ]);
   return (
-    <div>Booking</div>
+    <>
+    <div className="row border-bottom border-secondary">
+      <div className="col-auto">
+      <ControlButton controlButtons={controlButtons} />
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-auto">
+        Title Add Btn Section
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-auto">
+        Filter Secton
+      </div>
+    </div>
+    </>
   )
 }
 
